86fdb9f77e156483ee753129b3ff9d82ba276c5c
Alex Aegwynn <1549777384@qq.com>
2018-12-27 09:14:39 +0800

    commi

--- Firefox/beastify/content_scripts/beastify.js
+++ Firefox/beastify/content_scripts/beastify.js
@@ -1,47 +1,22 @@
 (function () {
-    /**
-     * Check and set a global guard variable.
-     * If this content script is injected into the same page again,
-     * it will do nothing next time.
-     */
     if (window.hasRun) {
         return;
     }
     window.hasRun = true;
 
-    /**
-     * Given a URL to a beast image, remove all existing beasts, then
-     * create and style an IMG node pointing to
-     * that image, then insert the node into the document.
-     */
     function insertBeast() {
 
-        var testjq = $('#J_Reviews').find('.tm-rate').find('.rate-grid').find('table').find('.tm-rate-fulltxt');
+        // var testjq = $('#J_Reviews').find('.tm-rate').find('.rate-grid').find('table').find('.tm-rate-fulltxt');
+        var testjq = $('.tb-rev-item');
 
         var arrp = new Array();
 
         for (let index = 0; index < testjq.length; index++) {
             arrp[index] = testjq[index].innerText;
         }
-
         console.log(arrp);
-
-        // var test = document.getElementsByTagName('body')[0];
-        // var last = test.children.namedItem('wrapper')
-        //     .children.namedItem('head')
-        //     .children.namedItem('head_wrapper')
-        //     .children.namedItem('s_fm')
-        //     .firstElementChild.children.namedItem('form')
-        //     .children.namedItem('s_kw_wrap')
-        //     .children.namedItem('kw').value;
-
-        // console.log(last);
-        // document.body.appendChild(beastImage);
     }
 
-    /**
-     * Remove every beast from the page.
-     */
     function removeExistingBeasts() {
         let existingBeasts = document.querySelectorAll(".beastify-image");
         for (let beast of existingBeasts) {
@@ -49,10 +24,6 @@
         }
     }
 
-    /**
-     * Listen for messages from the background script.
-     * Call "beastify()" or "reset()".
-    */
     browser.runtime.onMessage.addListener((message) => {
         if (message.command === "beastify") {
             insertBeast();
@@ -60,5 +31,4 @@
             removeExistingBeasts();
         }
     });
-
 })();
